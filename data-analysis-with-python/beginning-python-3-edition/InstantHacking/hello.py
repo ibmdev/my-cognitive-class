b8a81400694353f29fe075f9276f13ad1b3c83e9
ibmdev <ibmdev102017@gmail.com>
2017-12-25 17:36:34 +0100

    Instant Hacking : the Basics

---
+++ data-analysis-with-python/beginning-python-3-edition/InstantHacking/hello.py
@@ -0,0 +1,19 @@
+__author__ = 'ibmdev'
+# Import of library cmath to manage Complex Numbers
+import cmath
+print('Hello, World')
+x = cmath.sqrt(-5)
+print("The square root of -5 yields : " + str(x))
+# Supply a value and assign it to a variable
+# firstname = input("What is your firstname ? ")
+# print("Your firstname is : " + firstname)
+# input("Thanks, click <enter> to finish the program")
+# Representation of strings with simple and double quotes
+print('My first string with simple quotes')
+print("My first string with double quotes")
+print('My second "string" with a quote into my sentence')
+# When we add a backslash character before a single/double quote, Python understands that the middle single quote is a character in the string and not the end of the string.
+print('My third string with an backslash character and simple quotes let\'s go ')
+print('My fourth string with an backslash character and double quotes \"Hello, World\" ')
+print('Let\'s say "Hello World" to Cozmo Robot"')
+# The next tutorial Concatenating Strings p 46
